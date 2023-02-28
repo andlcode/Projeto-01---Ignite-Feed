1e6e64816bdc8edbd95921a915f2a9658628a09c
André Luiz Duarte Cruz <44853890+andrelDuarte@users.noreply.github.com>
2023-02-28 17:37:01 -0300

    Os motores do React - Iterando no JSX

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,21 @@ const posts = [
     id: 1,
     author: {
       avatarUrl: 'https://github.com/diego3g.png',
-      name: 'Diego Fernandes',
-      role: 'CTO @Rocketseat'
+      name: 'André Luiz',
+      role: 'Software Developer'
     },
     content: [
       { type: 'paragraph', content: 'Fala galera 👋' },
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
       { type: 'link', content: 'jane.design/doctorcare' },
     ],
-    publishedAt: new Date('2022-05-03 20:00:00'),
+    publishedAt: new Date('2022-12-12 20:00:00'),
   },
   {
     id: 2,
     author: {
       avatarUrl: 'https://github.com/maykbrito.png',
-      name: 'Mayk Brito',
+      name: '',
       role: 'Educator @Rocketseat'
     },
     content: [
@@ -31,7 +31,7 @@ const posts = [
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
       { type: 'link', content: 'jane.design/doctorcare' },
     ],
-    publishedAt: new Date('2022-05-10 20:00:00'),
+    publishedAt: new Date('2022-10-10 20:00:00'),
   },
 ];
 
