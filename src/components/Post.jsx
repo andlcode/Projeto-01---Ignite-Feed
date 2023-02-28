fb97e235a3236fe77e1965a5fe42420c9ce62f71
André Luiz Duarte Cruz <44853890+andrelDuarte@users.noreply.github.com>
2023-02-28 14:40:51 -0300

    update post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -20,7 +20,7 @@ export function Post(props) {
 
 
 
-  <div className={styles.commentList}>
+  <div className={styles.content}>
         <p>Fala galeraa 👋</p>
         <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
         <p><a href="">jane.design/doctorcare</a></p>
